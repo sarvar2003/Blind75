f8620ec77bba82c4901ffac704ae45d006e9c120
sarvar2003 <saravarjuraev@gmail.com>
2025-02-25 13:35:58 +0100

    191. Number of 1 Bits

---
+++ BitManupulation/191.Numberof1Bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        
+        while n:
+            n &= n-1 
+            res += 1
+        
+        return res
+
+# Time: O(logn)
+# Space: O(1)
