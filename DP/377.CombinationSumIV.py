f595c2fde358cac55746884d50f2d5e448787794
sarvar2003 <saravarjuraev@gmail.com>
2025-03-02 20:28:22 +0100

    377. Combination Sum IV

--- DP/377.CombinationSumIV.py
+++ DP/377.CombinationSumIV.py
@@ -20,6 +20,19 @@ class Solution:
             return res
         
         return dp(target, 0)
+    
+    def combinationSum4II(self, nums: List[int], target: int) -> int:
+        dp = [0] * (target+1)
+        
+        for i in range(1, target+1):
+            for n in nums:
+                if i - n < 0:
+                    continue
+                if i - n == 0:
+                    dp[i] += 1
+                dp[i] += dp[i-n]
+        
+        return dp[target]
 
 # Time: O(n*t)
 # Space: O(t)
